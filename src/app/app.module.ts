6dae5de63ed8e48d6fb4dd9cc339c93de2dfb0b2
Vanessa <vlombard1998@gmail.com>
2024-02-08 16:29:03 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SanityDataComponent } from './sanity-data/sanity-data.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SanityDataComponent
+    SanityDataComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
